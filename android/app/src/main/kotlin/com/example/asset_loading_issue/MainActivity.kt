b8119dd701dd8ee98f1af097de1483ec8d22b134
Moneer R. Alhashim <61453768+moneer-muntazah@users.noreply.github.com>
2023-03-01 16:49:36 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/asset_loading_issue/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.asset_loading_issue
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/flutter_bird.png',
+        package: 'asset_loading_issue',),
+      ),
+    );
+  }
+}
